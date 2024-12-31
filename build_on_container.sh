a4e839ccc23ac2ae3d5a64704d8c27f0f057d52a
TaiseiIto <taisei1212@outlook.jp>
2024-12-31 21:52:49 +0900

    Build liumOS #1

--- build_on_container.sh
+++ build_on_container.sh
@@ -4,4 +4,7 @@ rustup component add rust-src
 make tools
 make prebuilt
 make
+rustup install nightly
+rustup override set nightly
+rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
 
