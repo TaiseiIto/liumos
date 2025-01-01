b38c2a38206bda913268d2bad7a5bba6372ff81f
TaiseiIto <taisei1212@outlook.jp>
2025-01-01 21:50:49 +0900

    Build liumOS #1

--- build_on_container.sh
+++ build_on_container.sh
@@ -2,10 +2,11 @@
 source /root/.cargo/env
 rustup toolchain install nightly-x86_64-unknown-linux-gnu
 rustup component add rust-src
-make tools
-make prebuilt
-make
 rustup install nightly
 rustup override set nightly
 rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
+make tools
+make prebuilt
+make
+make files
 
