af37a94f664be54316ba84c6044ddda226bd4a6e
TaiseiIto <taisei1212@outlook.jp>
2025-02-09 20:27:18 +0900

    Debug #1

--- build_on_container.sh
+++ build_on_container.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 source /root/.cargo/env
-rustup toolchain install nightly-x86_64-unknown-linux-gnu
-rustup component add rust-src
-rustup install nightly
-rustup override set nightly
-rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
+rustup toolchain install nightly-2021-06-27-x86_64-unknown-linux-gnu
+rustup default nightly-2021-06-27-x86_64-unknown-linux-gnu
+rustup component add rust-src --toolchain nightly-2021-06-27-x86_64-unknown-linux-gnu
 make tools
 make prebuilt
 make
