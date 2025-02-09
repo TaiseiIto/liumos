799df158324c259f997556a7bb39b0b1da19d1a2
TaiseiIto <taisei1212@outlook.jp>
2025-02-09 21:09:47 +0900

    Solve the bug #1

--- build_on_container.sh
+++ build_on_container.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 source /root/.cargo/env
-rustup toolchain install nightly-2021-06-27-x86_64-unknown-linux-gnu
-rustup default nightly-2021-06-27-x86_64-unknown-linux-gnu
-rustup component add rust-src --toolchain nightly-2021-06-27-x86_64-unknown-linux-gnu
+rustup toolchain install nightly-2022-04-29-x86_64-unknown-linux-gnu
+rustup default nightly-2022-04-29-x86_64-unknown-linux-gnu
+rustup component add rust-src --toolchain nightly-2022-04-29-x86_64-unknown-linux-gnu
 make tools
 make prebuilt
 make
